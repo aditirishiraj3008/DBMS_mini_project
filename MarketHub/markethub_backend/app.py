d9935abd070f6067b5f222cefc19c19357289a9c
Shanu48 <149718849+Shanu48@users.noreply.github.com>
2025-04-04 01:46:41 +0530

    Update app.py

--- MarketHub/markethub_backend/app.py
+++ MarketHub/markethub_backend/app.py
@@ -1,24 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from routes.auth_routes import auth_blueprint
 from routes.product_routes import product_blueprint
 
-# Initialize Flask App
 app = Flask(__name__)
 
 app.secret_key = "Shanu@04082005"
 
 # Enable CORS
-CORS(app, resources={r"/auth/*": {"origins": "http://localhost:*"}}) 
+CORS(app)
 
 # Register Blueprints
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
 app.register_blueprint(product_blueprint, url_prefix='/')
 
+# Serve product page
+@app.route('/products')
+def products():
+    return render_template("products.html")
 
-# Test Route
-@app.route('/')
-def home():
-    return {"message": "Server is running!"}, 200
 if __name__ == '__main__':
     app.run(debug=True)
